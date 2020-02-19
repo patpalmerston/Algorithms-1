ad42bdb3ed17c469996a93171685610abbe5ce27
Pat Palmerston <patpalmerston@gmail.com>
2020-02-19 13:05:48 -0800

    stock prices passes 3 out of 4 tests, bug on negative

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,26 @@
 
 import argparse
 
+# bubble sort solution
+
+
 def find_max_profit(prices):
-  pass
+    result = []
+    for i in range(len(prices)):
+        for j in range(1, len(prices)):
+            temp = prices[j] - prices[i]
+            print(prices[i], prices[j], temp)
+            result.append(temp)
+    return max(result)
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+        # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
