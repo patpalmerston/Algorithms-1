3dab593930381ac55d4ca2a37312e9141b29a88b
Pat Palmerston <patpalmerston@gmail.com>
2020-02-19 14:22:58 -0800

    small test version for cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,26 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
 def eating_cookies(n, cache=None):
-  pass
+  # the test case of three has 4 options, 0,1,2,3 which gives us our answer of 4. So any value of 0 needs to be 1
+    if n <= 0:
+        n = 0
+        return n + 1
+  # any value of one is returned as one, less than one returned as one
+    if n <= 2:
+        return n
+
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
