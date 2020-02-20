6ef351e85c5de572892b51d3457218a2070161f3
Pat Palmerston <patpalmerston@gmail.com>
2020-02-20 12:39:00 -0800

    recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,29 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = []
+    for i in ingredients:
+        for j in recipe:
+            if j not in ingredients:
+                return 0
+            if j == i:
+                print(j, recipe[j], i, ingredients[i])
+                if recipe[j] > ingredients[i]:
+                    return 0
+                else:
+                    value = ingredients[i] // recipe[j]
+                    # print(math.floor(value))
+                    batches.append(math.floor(value))
+
+    return min(batches)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
